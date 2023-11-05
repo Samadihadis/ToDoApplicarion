5602ec496af0eb27d5a369d6084dedb5f3e1c44e
h.samadi <samadi.hadis@gmail.com>
2023-11-05 09:01:23 +0330

    Add NavComponent

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.hadis.todoapplicarion"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.hadis.todoapplicarion"
---
+++ app/src/main/java/com/hadis/todoapplicarion/CurrentTodos.kt
@@ -0,0 +1,25 @@
+package com.hadis.todoapplicarion
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigation
+
+class CurrentTodos : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_current_todos, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val textView = view.findViewById<TextView>(R.id.textViewCurrentTodos)
+        textView.setOnClickListener{
+            Navigation.findNavController(textView).navigate(R.id.action_currentTodo_to_addTask)
+        }
+    }
+}
